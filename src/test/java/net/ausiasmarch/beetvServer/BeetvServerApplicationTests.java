d4c98b39b6ff62f97b50fa510eaee119339f4385
beapm <beapm1994@gmail.com>
2021-01-23 00:22:17 +0100

    #3 - Test inicial

--- src/test/java/net/ausiasmarch/beetvServer/BeetvServerApplicationTests.java
+++ src/test/java/net/ausiasmarch/beetvServer/BeetvServerApplicationTests.java
@@ -1,13 +1,17 @@
 package net.ausiasmarch.beetvServer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BeetvServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testAppController() {
+        AppController homeController = new AppController();
+        String result = homeController.info().getBody();
+        assertEquals(result, "Welcome to BeeTV Server");
+    }
 
 }
