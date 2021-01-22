740fb35977678d05815c3423064df49d9b0715e2
beapm <beapm1994@gmail.com>
2021-01-23 00:09:10 +0100

    #1 - Inicio de proyecto

---
+++ src/main/java/net/ausiasmarch/beetvServer/BeetvServerApplication.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.beetvServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetvServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeetvServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ausiasmarch/beetvServer/BeetvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.beetvServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeetvServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
