245c015d17cc13bf5a76204124d509872163ca53
beapm <beapm1994@gmail.com>
2021-01-26 20:15:03 +0100

    CapituloController - get, getall, count, create, update, delete, page + permisos

--- src/main/java/net/ausiasmarch/beetvServer/api/CapituloController.java
+++ src/main/java/net/ausiasmarch/beetvServer/api/CapituloController.java
@@ -5,10 +5,160 @@
  */
 package net.ausiasmarch.beetvServer.api;
 
-/**
- *
- * @author beapm
- */
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import net.ausiasmarch.beetvServer.entity.CapituloEntity;
+import net.ausiasmarch.beetvServer.entity.UsuarioEntity;
+import net.ausiasmarch.beetvServer.repository.CapituloRepository;
+import net.ausiasmarch.beetvServer.repository.TipousuarioRepository;
+import net.ausiasmarch.beetvServer.repository.UsuarioRepository;
+import net.ausiasmarch.beetvServer.service.FillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/capitulo")
 public class CapituloController {
+
+    @Autowired
+    HttpSession oHttpSession;
+
+    @Autowired
+    UsuarioRepository oUsuarioRepository;
+
+    @Autowired
+    TipousuarioRepository oTipousuarioRepository;
+
+    @Autowired
+    CapituloRepository oCapituloRepository;
+
+    @Autowired
+    FillService oFillService;
+
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable(value = "id") Long id) {
+
+        if (oCapituloRepository.existsById(id)) {
+            return new ResponseEntity<CapituloEntity>(oCapituloRepository.getOne(id), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<CapituloEntity>(oCapituloRepository.getOne(id), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> all() {
+        if (oCapituloRepository.count() <= 1000) {
+            return new ResponseEntity<List<CapituloEntity>>(oCapituloRepository.findAll(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.PAYLOAD_TOO_LARGE);
+        }
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<?> count() {
+        return new ResponseEntity<Long>(oCapituloRepository.count(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> create(@RequestBody CapituloEntity oCapituloEntity) { // solo puede el admin
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+
+                if (oCapituloEntity.getId() == null) {
+                    return new ResponseEntity<CapituloEntity>(oCapituloRepository.save(oCapituloEntity), HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<Long>(0L, HttpStatus.NOT_MODIFIED);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody CapituloEntity oCapituloEntity) { // solo puede el admin
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+                oCapituloEntity.setId(id);
+                if (oCapituloRepository.existsById(id)) {
+                    return new ResponseEntity<CapituloEntity>(oCapituloRepository.save(oCapituloEntity), HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<Long>(0L, HttpStatus.NOT_MODIFIED);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {  // solo puede el admin
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+                if (oCapituloRepository.existsById(id)) {
+                    oCapituloRepository.deleteById(id);
+                    return new ResponseEntity<Long>(id, HttpStatus.NOT_MODIFIED);
+                } else {
+                    return new ResponseEntity<Long>(0L, HttpStatus.OK);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<?> getPage(@PageableDefault(page = 0, size = 10, direction = Sort.Direction.ASC) Pageable oPageable) {
+
+        Page<CapituloEntity> oPage = oCapituloRepository.findAll(oPageable);
+        return new ResponseEntity<Page<CapituloEntity>>(oPage, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/fill/{amount}")
+    public ResponseEntity<?> fill(@PathVariable(value = "amount") Long amount) {
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) { 
+                return new ResponseEntity<Long>(oFillService.capituloFill(amount), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+
+    }
     
 }
