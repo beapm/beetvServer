03371eaefc9687ce042c7b2a26ba21c27367f93d
beapm <beapm1994@gmail.com>
2021-01-26 20:33:05 +0100

    SerieController - get, getall, count, create, update, delete, page + permisos

--- src/main/java/net/ausiasmarch/beetvServer/api/SerieController.java
+++ src/main/java/net/ausiasmarch/beetvServer/api/SerieController.java
@@ -5,10 +5,138 @@
  */
 package net.ausiasmarch.beetvServer.api;
 
-/**
- *
- * @author beapm
- */
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import net.ausiasmarch.beetvServer.entity.SerieEntity;
+import net.ausiasmarch.beetvServer.entity.UsuarioEntity;
+import net.ausiasmarch.beetvServer.repository.SerieRepository;
+import net.ausiasmarch.beetvServer.repository.TipousuarioRepository;
+import net.ausiasmarch.beetvServer.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/serie")
 public class SerieController {
-    
+
+    @Autowired
+    HttpSession oHttpSession;
+
+    @Autowired
+    UsuarioRepository oUsuarioRepository;
+
+    @Autowired
+    TipousuarioRepository oTipousuarioRepository;
+
+    @Autowired
+    SerieRepository oSerieRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable(value = "id") Long id) {
+
+        if (oSerieRepository.existsById(id)) {
+            return new ResponseEntity<SerieEntity>(oSerieRepository.getOne(id), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<SerieEntity>(oSerieRepository.getOne(id), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> all() {
+        if (oSerieRepository.count() <= 1000) {
+            return new ResponseEntity<List<SerieEntity>>(oSerieRepository.findAll(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.PAYLOAD_TOO_LARGE);
+        }
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<?> count() {
+        return new ResponseEntity<Long>(oSerieRepository.count(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> create(@RequestBody SerieEntity oSerieEntity) { // solo puede el admin
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+
+                if (oSerieEntity.getId() == null) {
+                    return new ResponseEntity<SerieEntity>(oSerieRepository.save(oSerieEntity), HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<Long>(0L, HttpStatus.NOT_MODIFIED);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody SerieEntity oSerieEntity) { // solo puede el admin
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+                oSerieEntity.setId(id);
+                if (oSerieRepository.existsById(id)) {
+                    return new ResponseEntity<SerieEntity>(oSerieRepository.save(oSerieEntity), HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<Long>(0L, HttpStatus.NOT_MODIFIED);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {  // solo puede el admin
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+                if (oSerieRepository.existsById(id)) {
+                    oSerieRepository.deleteById(id);
+                    return new ResponseEntity<Long>(id, HttpStatus.NOT_MODIFIED);
+                } else {
+                    return new ResponseEntity<Long>(0L, HttpStatus.OK);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<?> getPage(@PageableDefault(page = 0, size = 10, direction = Sort.Direction.ASC) Pageable oPageable) {
+
+        Page<SerieEntity> oPage = oSerieRepository.findAll(oPageable);
+        return new ResponseEntity<Page<SerieEntity>>(oPage, HttpStatus.OK);
+
+    }
+
 }
