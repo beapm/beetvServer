804f899d2326fdfbeac4fb21f58bd3e773b63247
beapm <beapm1994@gmail.com>
2021-01-28 09:16:04 +0100

    Arreglos en el FillService

--- src/main/java/net/ausiasmarch/beetvServer/service/FillService.java
+++ src/main/java/net/ausiasmarch/beetvServer/service/FillService.java
@@ -2,13 +2,26 @@ package net.ausiasmarch.beetvServer.service;
 
 import java.time.ZoneId;
 import java.util.Optional;
+import java.util.Random;
+import net.ausiasmarch.beetvServer.entity.ActuacionEntity;
 import net.ausiasmarch.beetvServer.entity.CapituloEntity;
+import net.ausiasmarch.beetvServer.entity.ComentarioEntity;
 import net.ausiasmarch.beetvServer.entity.GeneroEntity;
+import net.ausiasmarch.beetvServer.entity.LikesEntity;
+import net.ausiasmarch.beetvServer.entity.PersonajeEntity;
+import net.ausiasmarch.beetvServer.entity.PuntuacionserieEntity;
+import net.ausiasmarch.beetvServer.entity.TemporadaEntity;
 import net.ausiasmarch.beetvServer.entity.TipousuarioEntity;
 import net.ausiasmarch.beetvServer.entity.UsuarioEntity;
 import net.ausiasmarch.beetvServer.helper.RandomHelper;
+import net.ausiasmarch.beetvServer.repository.ActuacionRepository;
 import net.ausiasmarch.beetvServer.repository.CapituloRepository;
+import net.ausiasmarch.beetvServer.repository.ComentarioRepository;
 import net.ausiasmarch.beetvServer.repository.GeneroRepository;
+import net.ausiasmarch.beetvServer.repository.LikesRepository;
+import net.ausiasmarch.beetvServer.repository.PersonajeRepository;
+import net.ausiasmarch.beetvServer.repository.PuntuacionserieRepository;
+import net.ausiasmarch.beetvServer.repository.TemporadaRepository;
 import net.ausiasmarch.beetvServer.repository.TipousuarioRepository;
 import net.ausiasmarch.beetvServer.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +35,30 @@ public class FillService {
 
     @Autowired
     UsuarioRepository oUsuarioRepository;
-    
+
     @Autowired
     CapituloRepository oCapituloRepository;
-    
+
     @Autowired
     GeneroRepository oGeneroRepository;
+
+    @Autowired
+    TemporadaRepository oTemporadaRepository;
+
+    @Autowired
+    PersonajeRepository oPersonajeRepository;
+
+    @Autowired
+    ActuacionRepository oActuacionRepository;
+
+    @Autowired
+    PuntuacionserieRepository oPuntuacionserieRepository;
+
+    @Autowired
+    LikesRepository oLikesRepository;
+
+    @Autowired
+    ComentarioRepository oComentarioRepository;
 
     public Long tipousuarioFill() {
 
@@ -79,6 +110,79 @@ public class FillService {
             oUsuarioEntity.setId_file(1L);
 
             oUsuarioRepository.save(oUsuarioEntity);
+        }
+        return cantidad;
+    }
+
+    public Long generoFill() {
+
+        GeneroEntity oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Animación");
+        oGeneroRepository.save(oGeneroEntity);
+
+        oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Ciencia Ficción");
+        oGeneroRepository.save(oGeneroEntity);
+
+        oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Comedia");
+        oGeneroRepository.save(oGeneroEntity);
+
+        oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Thriller");
+        oGeneroRepository.save(oGeneroEntity);
+
+        oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Drama");
+        oGeneroRepository.save(oGeneroEntity);
+
+        oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Acción y Aventuras");
+        oGeneroRepository.save(oGeneroEntity);
+
+        oGeneroEntity = new GeneroEntity();
+        oGeneroEntity.setNombre("Crimen");
+        oGeneroRepository.save(oGeneroEntity);
+
+        return 7L;
+    }
+
+    public Long temporadaFill(Long cantidad) {
+        for (int i = 1; i <= cantidad; i++) {
+            TemporadaEntity oTemporadaEntity = new TemporadaEntity();
+
+            oTemporadaEntity.setFecha_inicio(RandomHelper.getRadomDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+            oTemporadaEntity.setFecha_fin(RandomHelper.getRadomDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+        //    oTemporadaEntity.setId_serie(Long.valueOf(RandomHelper.getRandomInt(1, 15)));
+            oTemporadaEntity.setId_file(1L);
+
+            oTemporadaRepository.save(oTemporadaEntity);
+        }
+        return cantidad;
+    }
+
+    public Long personajeFill(Long cantidad) {
+
+        String[] nombres = {"Samantha", "Flavio", "Claudia", "Cristina", "Meredith", "Paula", "Raquel", "Alex",
+            "Guillermo", "Victoria", "Charles", "Carla", "Carlos", "Anya", "John", "Rachel", "Jennifer",
+            "Pablo", "Richard", "Tom"};
+        String[] apellidos = {"Brooks", "González", "Smith", "Brown", "Taylor", "Pérez", "Sánchez", "García",
+            "Fernández", "Rodríguez", "Cox", "Howard", "Anniston", "Murphy", "Hernández", "Bradford", "Williams",
+            "Díaz", "Navarro", "Vázquez"};
+
+        for (int i = 1; i <= cantidad; i++) {
+
+            PersonajeEntity oPersonajeEntity = new PersonajeEntity();
+
+            String nombre = nombres[(int) (Math.floor(Math.random() * ((nombres.length - 1) - 0 + 1) + 0))];
+            String apellido1 = apellidos[(int) (Math.floor(Math.random() * ((apellidos.length - 1) - 0 + 1) + 0))];
+            String apellido2 = apellidos[(int) (Math.floor(Math.random() * ((apellidos.length - 1) - 0 + 1) + 0))];
+            oPersonajeEntity.setNombre(nombre);
+            oPersonajeEntity.setApellido1(apellido1);
+            oPersonajeEntity.setApellido2(apellido2);
+            oPersonajeEntity.setId_file(1L);
+
+            oPersonajeRepository.save(oPersonajeEntity);
         }
         return cantidad;
     }
@@ -170,58 +274,156 @@ public class FillService {
         return nombre;
     }
 
+    public String getSinopsis01() {
+        String nombre = "";
+        switch (RandomHelper.getRandomInt(1, 5)) {
+            case 1:
+                nombre = "En este capítulo ";
+                break;
+            case 2:
+                nombre = "En el episodio de esta semana ";
+                break;
+            case 3:
+                nombre = "En esta nueva entrega ";
+                break;
+            case 4:
+                nombre = "Tras el episodio anterior, en el nuevo  ";
+                break;
+            case 5:
+                nombre = "La historia continúa y ";
+                break;
+        }
+        return nombre;
+    }
+
+    public String getSinopsis02() {
+        String nombre = "";
+        switch (RandomHelper.getRandomInt(1, 8)) {
+            case 1:
+                nombre = "los personajes tendrán que enfrentarse a un nuevo reto.";
+                break;
+            case 2:
+                nombre = "conoceremos uno de los misterios más sorprendentes de la serie.";
+                break;
+            case 3:
+                nombre = "se produce un altercado que no dejará indiferente a nadie.";
+                break;
+            case 4:
+                nombre = "dos de los personajes más importantes de la serie tendrán un encuentro inolvidable.";
+                break;
+            case 5:
+                nombre = "la protagonista vivirá una situación de vida o muerte.";
+                break;
+            case 6:
+                nombre = "sabremos por fin la identidad del asesino.";
+                break;
+            case 7:
+                nombre = "conoceremos la infancia de los protagonistas gracias a distintos flashbacks.";
+                break;
+            case 8:
+                nombre = "llegamos al especial navideño con muchas incógnitas por resolver.";
+                break;
+        }
+        return nombre;
+    }
+
+    public String getSinopsis03() {
+        String nombre = "";
+        switch (RandomHelper.getRandomInt(1, 5)) {
+            case 1:
+                nombre += " Al final hay una revelación que nadie se espera.";
+                break;
+            case 2:
+                nombre += " Habrá que esperar al próximo episodio para ver cómo continúa la historia.";
+                break;
+            case 3:
+                nombre += " ¿Qué pasará próximamente?";
+                break;
+            case 4:
+                nombre += " El desenlace abierto no dejará indiferente a nadie";
+                break;
+            case 5:
+                nombre += " Presta atención a los detalles de la escena final.";
+                break;
+        }
+        return nombre;
+    }
+
     public Long capituloFill(Long cantidad) {
         for (int i = 1; i <= cantidad; i++) {
             CapituloEntity oCapituloEntity = new CapituloEntity();
 
             oCapituloEntity.setNombre(this.getCapitulo01() + this.getCapitulo02() + this.getCapitulo03());
-            oCapituloEntity.setSinopsis_capitulo("Sinopsis");
+            oCapituloEntity.setSinopsis_capitulo(this.getSinopsis01() + this.getSinopsis02() + this.getSinopsis03());
             oCapituloEntity.setFecha_emision(RandomHelper.getRadomDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
             oCapituloEntity.setDuracion(RandomHelper.getRandomInt(21, 78));
-            oCapituloEntity.setId_temporada(1L);
+        //    oCapituloEntity.setId_temporada(Long.valueOf(RandomHelper.getRandomInt(1, 100)));
             oCapituloEntity.setId_file(1L);
-            
-        oCapituloRepository.save(oCapituloEntity);
+
+            oCapituloRepository.save(oCapituloEntity);
         }
         return cantidad;
     }
 
-    public Long generoFill () {
-        
-        GeneroEntity oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Animación");
-        oGeneroRepository.save(oGeneroEntity);
+    public Long actuacionFill(Long cantidad) {
+        for (int i = 1; i <= cantidad; i++) {
+            ActuacionEntity oActuacionEntity = new ActuacionEntity();
 
-        
-        oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Cienia Ficción");
-        oGeneroRepository.save(oGeneroEntity);
+        //    oActuacionEntity.setId_capitulo(Long.valueOf(RandomHelper.getRandomInt(1, 500)));
+        //    oActuacionEntity.setId_personaje(Long.valueOf(RandomHelper.getRandomInt(1, 300)));
 
-        
-        oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Comedia");
-        oGeneroRepository.save(oGeneroEntity);
+            oActuacionRepository.save(oActuacionEntity);
+        }
+        return cantidad;
+    }
 
-        
-        oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Thriller");
-        oGeneroRepository.save(oGeneroEntity);
+    public Long puntuacionserieFill(Long cantidad) {
+        for (int i = 1; i <= cantidad; i++) {
+            PuntuacionserieEntity oPuntuacionserieEntity = new PuntuacionserieEntity();
 
-        
-        oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Drama");
-        oGeneroRepository.save(oGeneroEntity);
+            oPuntuacionserieEntity.setId_serie(Long.valueOf(RandomHelper.getRandomInt(1, 20)));
+            oPuntuacionserieEntity.setId_usuario(Long.valueOf(RandomHelper.getRandomInt(1, 200)));
+            oPuntuacionserieEntity.setPuntuacion(RandomHelper.getRandomInt(1, 5));
 
-        
-        oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Acción y Aventuras");
-        oGeneroRepository.save(oGeneroEntity);
+            oPuntuacionserieRepository.save(oPuntuacionserieEntity);
+        }
+        return cantidad;
+    }
 
-        
-        oGeneroEntity = new GeneroEntity();
-        oGeneroEntity.setNombre("Crimen");
-        oGeneroRepository.save(oGeneroEntity);
+    public Long likesFill(Long cantidad) {
+        for (int i = 1; i <= cantidad; i++) {
 
-        return 7L;
+            LikesEntity oLikesEntity = new LikesEntity();
+
+        //    oLikesEntity.setId_comentario(Long.valueOf(RandomHelper.getRandomInt(1, 200)));
+        //    oLikesEntity.setId_usuario(Long.valueOf(RandomHelper.getRandomInt(1, 200)));
+
+            Random rand = new Random(); // para obtener un booleano aleatorio
+            Boolean value = rand.nextBoolean();
+
+            oLikesEntity.setLike_type(value);
+
+            oLikesRepository.save(oLikesEntity);
+        }
+        return cantidad;
+    }
+
+    public Long comentarioFill(Long cantidad) {
+
+        String[] coments = {"Muy buen capítulo.", "No me ha gustado nada.", "¡Gran final de temporada!", "Me habría gustado que tuviese un poco más de acción...", 
+            "¡Ojalá se salve!", "Muy divertido, ¡me he reído mucho!"};
+
+        for (int i = 1; i <= cantidad; i++) {
+            ComentarioEntity oComentarioEntity = new ComentarioEntity();
+
+            String coment = coments[(int) (Math.floor(Math.random() * ((coments.length - 1) - 0 + 1) + 0))];
+            
+           // oComentarioEntity.setId_capitulo(Long.valueOf(RandomHelper.getRandomInt(1, 500)));
+           // oComentarioEntity.setId_usuario(Long.valueOf(RandomHelper.getRandomInt(1, 300)));
+            oComentarioEntity.setTexto(coment);
+
+            oComentarioRepository.save(oComentarioEntity);
+        }
+        return cantidad;
     }
 }
