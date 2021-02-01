359922c9c9bb809424a902c73437da4372b6c181
beapm <beapm1994@gmail.com>
2021-02-01 20:55:15 +0100

    Relaciones repositorios: capitulo x temporada

--- src/main/java/net/ausiasmarch/beetvServer/repository/CapituloRepository.java
+++ src/main/java/net/ausiasmarch/beetvServer/repository/CapituloRepository.java
@@ -6,15 +6,15 @@
 package net.ausiasmarch.beetvServer.repository;
 
 import net.ausiasmarch.beetvServer.entity.CapituloEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author beapm
- */
-
 @Repository
-public interface CapituloRepository extends JpaRepository<CapituloEntity, Long>  {
-    
+public interface CapituloRepository extends JpaRepository<CapituloEntity, Long> {
+
+    @Query(value = "SELECT * FROM capitulo c WHERE c.id_temporada= :id_temporada", nativeQuery = true)
+    Page<CapituloEntity> findByCapituloXTemporada(Long id_temporada, Pageable pageable);
 }
