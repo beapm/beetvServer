e976d3e31387099c8421d08743939501370dcf27
beapm <beapm1994@gmail.com>
2021-02-01 20:54:42 +0100

    Relaciones repositorios: likes x usuario y likes x capitulo

--- src/main/java/net/ausiasmarch/beetvServer/repository/LikesRepository.java
+++ src/main/java/net/ausiasmarch/beetvServer/repository/LikesRepository.java
@@ -6,15 +6,18 @@
 package net.ausiasmarch.beetvServer.repository;
 
 import net.ausiasmarch.beetvServer.entity.LikesEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author beapm
- */
-
 @Repository
-public interface LikesRepository extends JpaRepository<LikesEntity, Long>  {
-    
+public interface LikesRepository extends JpaRepository<LikesEntity, Long> {
+
+    @Query(value = "SELECT * FROM likes l WHERE l.id_usuario= :id_usuario", nativeQuery = true)
+    Page<LikesEntity> findByLikesXUsuario(Long id_usuario, Pageable pageable);
+
+    @Query(value = "SELECT * FROM likes l WHERE l.id_capitulo= :id_capitulo", nativeQuery = true)
+    Page<LikesEntity> findByLikesXCapitulo(Long id_capitulo, Pageable pageable);
 }
