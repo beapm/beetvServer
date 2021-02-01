b27e0d5a2f13d0cdb0d998312bbee917b8267045
beapm <beapm1994@gmail.com>
2021-02-01 20:54:21 +0100

    Relaciones repositorios: puntuacion x usuario y puntuacion x serie

--- src/main/java/net/ausiasmarch/beetvServer/repository/PuntuacionserieRepository.java
+++ src/main/java/net/ausiasmarch/beetvServer/repository/PuntuacionserieRepository.java
@@ -6,15 +6,18 @@
 package net.ausiasmarch.beetvServer.repository;
 
 import net.ausiasmarch.beetvServer.entity.PuntuacionserieEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-/**
- *
- * @author beapm
- */
 
 @Repository
 public interface PuntuacionserieRepository extends JpaRepository<PuntuacionserieEntity, Long> {
-    
+
+    @Query(value = "SELECT * FROM puntuacionserie p WHERE p.id_usuario= :id_usuario", nativeQuery = true)
+    Page<PuntuacionserieEntity> findByPuntuacionXUsuario(Long id_usuario, Pageable pageable);
+
+    @Query(value = "SELECT * FROM puntuacionserie p WHERE p.id_serie= :id_serie", nativeQuery = true)
+    Page<PuntuacionserieEntity> findByPuntuacionXSerie(Long id_serie, Pageable pageable);
 }
