b3a7070c0d12adf3ead74f4525621edc90fa8bca
beapm <beapm1994@gmail.com>
2021-02-01 20:54:57 +0100

    Relaciones repositorios: comentario x usuario y comentario x capitulo

--- src/main/java/net/ausiasmarch/beetvServer/repository/ComentarioRepository.java
+++ src/main/java/net/ausiasmarch/beetvServer/repository/ComentarioRepository.java
@@ -6,15 +6,18 @@
 package net.ausiasmarch.beetvServer.repository;
 
 import net.ausiasmarch.beetvServer.entity.ComentarioEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author beapm
- */
-
 @Repository
-public interface ComentarioRepository extends JpaRepository<ComentarioEntity, Long>  {
-    
+public interface ComentarioRepository extends JpaRepository<ComentarioEntity, Long> {
+
+    @Query(value = "SELECT * FROM comentario c WHERE c.id_usuario= :id_usuario", nativeQuery = true)
+    Page<ComentarioEntity> findByComentarioXUsuario(Long id_usuario, Pageable pageable);
+
+    @Query(value = "SELECT * FROM comentario c WHERE c.id_capitulo= :id_capitulo", nativeQuery = true)
+    Page<ComentarioEntity> findByComentarioXCapitulo(Long id_capitulo, Pageable pageable);
 }
