ac0c8f5464a48e6bb8626f0644decc816063020d
beapm <beapm1994@gmail.com>
2021-02-01 20:53:26 +0100

    Relaciones repositorios: serie x genero

--- src/main/java/net/ausiasmarch/beetvServer/repository/SerieRepository.java
+++ src/main/java/net/ausiasmarch/beetvServer/repository/SerieRepository.java
@@ -5,16 +5,18 @@
  */
 package net.ausiasmarch.beetvServer.repository;
 
+import net.ausiasmarch.beetvServer.entity.GeneroEntity;
 import net.ausiasmarch.beetvServer.entity.SerieEntity;
+import net.ausiasmarch.beetvServer.entity.UsuarioEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author beapm
- */
-
 @Repository
-public interface SerieRepository extends JpaRepository<SerieEntity, Long>  {
-    
+public interface SerieRepository extends JpaRepository<SerieEntity, Long> {
+
+    @Query(value = "SELECT * FROM serie s WHERE s.id_genero= :id_genero", nativeQuery = true)
+    Page<SerieEntity> findBySerieXGenero(Long id_genero, Pageable pageable);
 }
